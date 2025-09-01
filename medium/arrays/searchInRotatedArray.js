863f8add5daac82e2aaefe9727baa98347beac6e
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2025-09-01 20:09:15 +0530

    medium - searching in a left rotated array

---
+++ medium/arrays/searchInRotatedArray.js
@@ -0,0 +1,37 @@
+// link to the problem - https://leetcode.com/problems/search-in-rotated-sorted-array/description/?envType=problem-list-v2&envId=array
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // Check if left side is sorted
+    if (nums[left] <= nums[mid]) {
+      // Target in the left sorted half?
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // Right half is sorted
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
